9c51adad31ea70fddf13182b95900d568b8a03ed
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-09-15 12:53:28 -0400

    updating
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch
 import click
 
 DATASET = 'aisquared/cot-ensemble-prompts'
-MODEL_ID = 'gpt2-xl'
+MODEL_ID = 'meta-llama/Llama-2-13b-hf'
 QUESTION_KEY = 'Question:'
 THOUGHT_KEY = 'Thought:'
 ANSWER_KEY = 'Final Answer:'
@@ -68,7 +68,7 @@ def get_model_and_tokenizer(model_id = MODEL_ID, gradient_checkpointing = False)
             'additional_special_tokens' : [QUESTION_KEY, THOUGHT_KEY, ANSWER_KEY, END_KEY]
         }
     )
-    model.resize_token_embeddings(len(tokenizer))
+    model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of = 8)
 
     model = prepare_model_for_kbit_training(model)
 
